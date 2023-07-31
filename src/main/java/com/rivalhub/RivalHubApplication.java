14ec7a04743c22d834e94efc7a1de58cb7ce1ed8
Mateusz Szkudlarek <mateusz.szkudlarek55@gmail.com>
2023-07-31 11:50:51 +0200

    init project

---
+++ src/main/java/com/rivalhub/RivalHubApplication.java
@@ -0,0 +1,13 @@
+package com.rivalhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RivalHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RivalHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rivalhub/RivalHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rivalhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RivalHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
