946a4cb8911068f41e765a6f221b8757cb7e1755
JakuBuszta <buszta-jakub@wp.pl>
2023-07-31 15:50:58 +0200

    RIV-30 CRUD-organizacji

--- src/main/java/com/rivalhub/organization/OrganizationController.java
+++ src/main/java/com/rivalhub/organization/OrganizationController.java
@@ -15,7 +15,7 @@ import java.util.NoSuchElementException;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/organization")
+@RequestMapping("/organizations")
 @AllArgsConstructor
 public class OrganizationController {
 
