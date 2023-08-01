48b4e939f7c07a9226444d467bec477801ef8efc
maciejOlszewski01 <66844898+maciejOlszewski01@users.noreply.github.com>
2023-08-01 14:17:13 +0200

    Added email sender service

--- src/main/java/com/rivalhub/email/EmailService.java
+++ src/main/java/com/rivalhub/email/EmailService.java
@@ -30,8 +30,6 @@ public class EmailService {
     @Value("${spring.mail.username}")
     private String sender;
 
-
-
     public void sendSimpleMessage(String receiver,String subject,String message){
         SimpleMailMessage mailMessage = new SimpleMailMessage();
         mailMessage.setText(message);
@@ -40,34 +38,4 @@ public class EmailService {
         mailMessage.setSubject(subject);
         javaMailSender.send(mailMessage);
     }
-
-    public void sendAttachmentMessage(String receiver,String subject,String messageBody, String pathToFile){
-
-        MimeMessage message = javaMailSender.createMimeMessage();
-        try {
-            MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(message, true);
-            mimeMessageHelper.setFrom(sender);
-            mimeMessageHelper.setTo(receiver);
-            mimeMessageHelper.setSubject(subject);
-
-            MimeBodyPart attachmentPart = new MimeBodyPart();
-            attachmentPart.attachFile(new File(pathToFile));
-
-            BodyPart messageBodyPart = new MimeBodyPart();
-            messageBodyPart.setText(messageBody);
-
-            Multipart multipart = new MimeMultipart();
-
-            multipart.addBodyPart(messageBodyPart);
-            multipart.addBodyPart(attachmentPart);
-            message.setContent(multipart);
-            javaMailSender.send(message);
-        }catch(IOException e){
-
-        }catch (MessagingException e){
-
-        }
-    }
-
-
 }
