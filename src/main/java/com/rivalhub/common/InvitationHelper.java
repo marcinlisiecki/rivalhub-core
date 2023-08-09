b4ab0824c56f46157e5ea4dbddb96fdc4a6b30ff
Mateusz Szkudlarek <mateusz.szkudlarek55@gmail.com>
2023-08-09 13:34:39 +0200

    RIV-97 change url to send correct invitation link

--- src/main/java/com/rivalhub/common/InvitationHelper.java
+++ src/main/java/com/rivalhub/common/InvitationHelper.java
@@ -7,9 +7,11 @@ public class InvitationHelper {
     public static String createInvitationLink(OrganizationDTO organizationDTO){
         StringBuilder builder = new StringBuilder();
         builder.setLength(0);
-        ServletUriComponentsBuilder uri = ServletUriComponentsBuilder.fromCurrentRequest();
-        uri.replacePath("");
-        builder.append(uri.toUriString()).append("/")
+//        ServletUriComponentsBuilder uri = ServletUriComponentsBuilder.fromCurrentRequest();
+//        uri.replacePath("");
+        String frontUrl = "http://localhost:4200";
+        builder.append(frontUrl)
+                .append("/organizations/")
                 .append(organizationDTO.getId())
                 .append("/invitation/")
                 .append(organizationDTO.getInvitationHash());
