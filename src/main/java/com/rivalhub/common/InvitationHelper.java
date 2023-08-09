d77cf1639a328ba292724d3ddf4daf0af8ce7273
Mateusz Szkudlarek <mateusz.szkudlarek55@gmail.com>
2023-08-09 13:35:30 +0200

    RIV-97 erase comments

--- src/main/java/com/rivalhub/common/InvitationHelper.java
+++ src/main/java/com/rivalhub/common/InvitationHelper.java
@@ -1,14 +1,11 @@
 package com.rivalhub.common;
 
 import com.rivalhub.organization.OrganizationDTO;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 public class InvitationHelper {
     public static String createInvitationLink(OrganizationDTO organizationDTO){
         StringBuilder builder = new StringBuilder();
         builder.setLength(0);
-//        ServletUriComponentsBuilder uri = ServletUriComponentsBuilder.fromCurrentRequest();
-//        uri.replacePath("");
         String frontUrl = "http://localhost:4200";
         builder.append(frontUrl)
                 .append("/organizations/")
