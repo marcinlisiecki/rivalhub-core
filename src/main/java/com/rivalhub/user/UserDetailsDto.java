3176289720cbbfc47a2e884be512b273f16e29af
JakuBuszta <buszta-jakub@wp.pl>
2023-08-11 14:29:54 +0200

    RIV-126 add all users in organization end point

--- src/main/java/com/rivalhub/user/UserDetailsDto.java
+++ src/main/java/com/rivalhub/user/UserDetailsDto.java
@@ -1,5 +1,6 @@
 package com.rivalhub.user;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -7,19 +8,11 @@ import java.time.LocalDateTime;
 
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 public class UserDetailsDto {
-
     private long id;
     private String name;
     private String email;
     private String profilePictureUrl;
     private LocalDateTime activationTime;
-
-    public UserDetailsDto(Long id, String name, String email, String profilePictureUrl, LocalDateTime activationTime) {
-        this.id = id;
-        this.name = name;
-        this.email = email;
-        this.profilePictureUrl = profilePictureUrl;
-        this.activationTime = activationTime;
-    }
 }
