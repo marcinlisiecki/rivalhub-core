baa7d0e86fd1c8d1f9389a75dc6061c2a97faf2b
JakuBuszta <buszta-jakub@wp.pl>
2023-08-23 12:06:59 +0200

    RIV-213 patch user

--- src/main/java/com/rivalhub/user/profile/ProfileController.java
+++ src/main/java/com/rivalhub/user/profile/ProfileController.java
@@ -1,10 +1,13 @@
 package com.rivalhub.user.profile;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.github.fge.jsonpatch.JsonPatchException;
+import com.github.fge.jsonpatch.mergepatch.JsonMergePatch;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequiredArgsConstructor
@@ -19,5 +22,12 @@ public class ProfileController {
     @GetMapping("/users/{id}/events")
     private ResponseEntity<?> sharedOrganizationEvents(@PathVariable Long id){
         return ResponseEntity.ok(profileService.getSharedOrganizationEvents(id));
+    }
+
+    @PatchMapping("/users")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    private ResponseEntity<?> updateUser(@RequestBody JsonMergePatch patch)
+            throws JsonPatchException, JsonProcessingException {
+        return ResponseEntity.ok(profileService.updateProfile(patch));
     }
 }
--- src/main/java/com/rivalhub/user/profile/ProfileService.java
+++ src/main/java/com/rivalhub/user/profile/ProfileService.java
@@ -1,8 +1,18 @@
 package com.rivalhub.user.profile;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.github.fge.jsonpatch.JsonPatchException;
+import com.github.fge.jsonpatch.mergepatch.JsonMergePatch;
+import com.rivalhub.common.AutoMapper;
+import com.rivalhub.common.MergePatcher;
+import com.rivalhub.organization.OrganizationDTO;
+import com.rivalhub.organization.OrganizationMapper;
+import com.rivalhub.organization.exception.OrganizationNotFoundException;
+import com.rivalhub.organization.validator.OrganizationSettingsValidator;
 import com.rivalhub.security.SecurityUtils;
 import com.rivalhub.user.UserAlreadyExistsException;
 import com.rivalhub.user.UserData;
+import com.rivalhub.user.UserDetailsDto;
 import com.rivalhub.user.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -16,6 +26,8 @@ public class ProfileService {
     private final UserRepository userRepository;
     private final UserProfileHelper userProfileHelper;
 
+    private final MergePatcher<UserDetailsDto> userDetailsDtoMergePatcher;
+    private final AutoMapper autoMapper;
     Set<ReservationInProfileDTO> getSharedOrganizationReservations(Long id) {
         var requestUser = SecurityUtils.getUserFromSecurityContext();
         UserData viewedUser = userRepository.findById(id)
@@ -30,5 +42,15 @@ public class ProfileService {
                 .orElseThrow(UserAlreadyExistsException::new);
 
         return userProfileHelper.getEventsInSharedOrganizations(requestUser, viewedUser);
+    }
+
+    public UserDetailsDto updateProfile(JsonMergePatch patch) throws JsonPatchException, JsonProcessingException {
+        var requestUser = SecurityUtils.getUserFromSecurityContext();
+
+        UserDetailsDto userDTO = autoMapper.mapToUserDetails(requestUser);
+        UserDetailsDto userDetailsDto = userDetailsDtoMergePatcher.patch(patch, userDTO, UserDetailsDto.class);
+        userRepository.save(autoMapper.mapToUserData(userDetailsDto));
+
+        return userDetailsDto;
     }
 }
