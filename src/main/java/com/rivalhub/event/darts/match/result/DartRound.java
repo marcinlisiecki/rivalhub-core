3ab7b3be6311c8800e9e96472e540850bb69f45e
maciejOlszewski01 <66844898+maciejOlszewski01@users.noreply.github.com>
2023-08-28 13:07:32 +0200

    RIV-248 add separated adding data

--- src/main/java/com/rivalhub/event/darts/match/result/DartRound.java
+++ src/main/java/com/rivalhub/event/darts/match/result/DartRound.java
@@ -13,6 +13,6 @@ public class DartRound {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @OneToMany(orphanRemoval = true, cascade = CascadeType.ALL)
+    @OneToMany(orphanRemoval = true, cascade = CascadeType.REMOVE)
     private List<SinglePlayerScoreInRound> singlePlayerScoreInRoundsList = new ArrayList<>();
 }
