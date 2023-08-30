aa4496950149fa863fa2564c797103d9e5832fbc
JakuBuszta <buszta-jakub@wp.pl>
2023-08-30 10:26:18 +0200

    RIV-264 add delete match

--- src/main/java/com/rivalhub/event/match/MatchApprovalService.java
+++ src/main/java/com/rivalhub/event/match/MatchApprovalService.java
@@ -16,7 +16,7 @@ public class MatchApprovalService {
     public static Map<Long, Boolean> prepareApprovalMap(MatchDto matchDto) {
         Map<Long,Boolean> userApproval = new HashMap<>();
         matchDto.getTeam1Ids().forEach(id-> userApproval.put(id,false));
-        matchDto.getTeam2Ids().forEach(id-> userApproval.put(id,false));
+        if (matchDto.getTeam2Ids() != null) matchDto.getTeam2Ids().forEach(id-> userApproval.put(id,false));
         return userApproval;
     }
 
